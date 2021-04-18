5e33967525cc14e2d8b51f22324e7500bb4ddbd7
Debanu Gupta <debanugupta@gmail.com>
2021-04-18 21:36:49 +0530

    initial push of 1st day 18042020 till 1hr :10 min

---
+++ project/react-app/src/components/DCandidateForm.js
@@ -0,0 +1,74 @@
+import { Grid, TextField } from "@material-ui/core";
+import React, {useState} from "react";
+
+const initialFieldValues = {
+    fullName: '',
+    mobile: '',
+    email: '',
+    age: '',
+    bloodGroup: '',
+    address: ''
+}
+
+
+
+const DCandidateForm = (props) => {
+    const [values,setValues] = useState(initialFieldValues)
+
+
+    const handleInputChange = e => {
+        const { name, value} = e.target
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+    return ( 
+        <form autoComplete="off" noValidate>
+            <Grid container>
+                <Grid item xs={6}>
+                    <TextField
+                        name="fullName"
+                        variant="outlined"
+                        label="Full Name"
+                        value={values.fullName}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        name="email"
+                        variant="outlined"
+                        label="Email"
+                        value={values.email}
+                        onChange={handleInputChange}
+                    />
+                    <div>bloodGroup</div>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        name="mobile"
+                        variant="outlined"
+                        label="Mobile"
+                        value={values.mobile}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        name="age"
+                        variant="outlined"
+                        label="Age"
+                        value={values.age}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        name="address"
+                        variant="outlined"
+                        label="Address"
+                        value={values.address}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+            </Grid>
+        </form>
+    )
+}
+
+export default DCandidateForm;
---
+++ project/react-app/src/components/DCandidates.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect}  from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions/dCandidate";
+import { Grid, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, withStyles } from "@material-ui/core";
+import DCandidateForm from "./DCandidateForm";
+
+const styles = theme =>({
+    root: {
+        "& .MuiTableCell-head": {
+            fontsize: "1.25rem"
+        }
+    },
+    paper :{
+        margin: theme.spacing(2),
+        padding: theme.spacing(2)
+    }
+})
+
+const DCandidates = ({classes,...props}) => {
+    // const [x, setX]= useState(0)
+    // setX(5)
+
+    useEffect(()=> {
+        props.fetchAllCandidates()
+    },[])//componentDidMount
+
+    return (
+        <Paper className={classes.paper} elevation={3}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <DCandidateForm></DCandidateForm>
+                </Grid>
+                <Grid item xs={6}>
+                    <TableContainer>
+                        <Table>
+                            <TableHead className={classes.root}>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Mobile</TableCell>
+                                    <TableCell>Blood Group</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    props.dCandidateList.map((record,index)=>{
+                                        return (<TableRow key={index} hover>
+                                            <TableCell>{record.fullName}</TableCell>
+                                            <TableCell>{record.mobile}</TableCell>
+                                            <TableCell>{record.bloodGroup}</TableCell>
+                                        </TableRow>
+
+                                        )
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
+
+const mapStateToProps = state =>({
+        dCandidateList:state.dCandidate.list
+})
+
+const mapActionToProps = {
+    fetchAllCandidates: actions.fetchall
+}
+
+export default connect(mapStateToProps,mapActionToProps)(withStyles(styles)(DCandidates));
